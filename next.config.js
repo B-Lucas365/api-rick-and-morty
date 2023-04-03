aa304439284d9319011c2c3022768de19ebeec34
Lucas Renan <lucasrenan365@outlook.com>
2023-04-02 22:38:03 -0300

    modificando arquivo de configuracao do next para resolver erro de deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: true,
+  reactStrictMode: false,
+  trailingSlash: true,
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js']
 }
 
 module.exports = nextConfig
